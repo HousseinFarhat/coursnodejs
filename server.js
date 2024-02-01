005ce7534a8ba6b31d4b150033bc5dffc65a8cd2
HousseinFarhat <houssein.farhat@outlook.com>
2024-02-01 19:11:20 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const http = require('http')
+
+const server = http.createServer((req,res)=>{
+   if(req.url=='/home'){
+    res.statusCode=200
+    res.write('welcome to home page')
+   }
+   else if(req.url=='/contact'){
+    res.statusCode=200
+    res.write('welcome to contact page')
+   }
+   else if(req.url=='/about'){
+    res.statusCode=200
+    res.write('welcome to about page')
+   }
+   else{
+    res.statusCode=404
+    res.write('page not found')
+   }
+
+  
+   res.end()
+})
+
+server.listen(5000,()=>{console.log('server running')})
